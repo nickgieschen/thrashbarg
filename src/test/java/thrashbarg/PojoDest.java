901eeaee6020c604939e4111839829440cc20e0e
Nick Gieschen <nickgieschen@gmail.com>
2016-12-26 18:59:39 -0800

    Initial commit: basic pojo and poko mapping

---
+++ src/test/java/thrashbarg/PojoDest.java
@@ -0,0 +1,22 @@
+package thrashbarg;
+
+public class PojoDest {
+    private int x1;
+    private int y;
+
+    public Integer getX1() {
+        return x1;
+    }
+
+    public void setX1(int x) {
+        this.x1 = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+}
